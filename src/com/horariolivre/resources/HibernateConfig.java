c9a7b0c8eb43b3c54072d85f50e820fd8495d8da
Kleber Mota <klebermo@gmail.com>
2014-10-30 21:54:11 -0300

    updating HibernateConfig

--- src/com/horariolivre/resources/HibernateConfig.java
+++ src/com/horariolivre/resources/HibernateConfig.java
@@ -1,5 +1,6 @@
 package com.horariolivre.resources;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -38,22 +39,30 @@ public class HibernateConfig {
    public DataSource restDataSource() {
       BasicDataSource dataSource = new BasicDataSource();
       
-		Properties props = new Properties();
-		FileInputStream fos;
-		try {
-			fos = new FileInputStream( this.property_file );
-			props.load(fos);
-			fos.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-      dataSource.setDriverClassName(props.getProperty("jdbc.Classname"));
-      dataSource.setUrl(props.getProperty("jdbc.url"));
-      dataSource.setUsername(props.getProperty("jdbc.user"));
-      dataSource.setPassword(props.getProperty("jdbc.pass"));
+      Properties props = new Properties();
+      File yourFile = new File( this.property_file );
+      if(yourFile.exists()) {
+    	  FileInputStream fos;
+    	  try {
+    		  fos = new FileInputStream( this.property_file );
+    		  props.load(fos);
+    		  fos.close();
+    		  
+    		  dataSource.setDriverClassName(props.getProperty("jdbc.Classname"));
+    		  dataSource.setUrl(props.getProperty("jdbc.url"));
+    		  dataSource.setUsername(props.getProperty("jdbc.user"));
+    		  dataSource.setPassword(props.getProperty("jdbc.pass"));
+		  } catch (FileNotFoundException e) {
+			  e.printStackTrace();
+		  } catch (IOException e) {
+			  e.printStackTrace();
+		  }
+	  } else {
+		  dataSource.setDriverClassName("org.postgresql.Driver");
+		  dataSource.setUrl("jdbc:postgresql://localhost:5432/bogus_database?charSet=LATIN1");
+		  dataSource.setUsername("bogus_user");
+		  dataSource.setPassword("bogus_pwd");
+	  }
 
       return dataSource;
    }
@@ -68,20 +77,27 @@ public class HibernateConfig {
    Properties hibernateProperties() {
 	   Properties props = new Properties();
 	   
-	   FileInputStream fos;
-	   try {
-		   fos = new FileInputStream( this.property_file );
-		   props.load(fos);
-		   fos.close();
-	   } catch (FileNotFoundException e) {
-		   e.printStackTrace();
-	   } catch (IOException e) {
-		   e.printStackTrace();
+	   File yourFile = new File( this.property_file );
+	   if(yourFile.exists()) {
+		   FileInputStream fos;
+		   try {
+			   fos = new FileInputStream( this.property_file );
+			   props.load(fos);
+			   fos.close();
+			   
+			   props.setProperty("hibernate.hbm2ddl.auto", props.getProperty("hibernate.hbm2ddl.auto"));
+			   props.setProperty("hibernate.show_sql", props.getProperty("hibernate.show_sql"));
+			   props.setProperty("hibernate.dialect", props.getProperty("hibernate.dialect"));
+		   } catch (FileNotFoundException e) {
+			   e.printStackTrace();
+		   } catch (IOException e) {
+			   e.printStackTrace();
+		   }
+	   } else {
+		   props.setProperty("hibernate.hbm2ddl.auto", "create");
+		   props.setProperty("hibernate.show_sql", "false");
+		   props.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
 	   }
-	   
-	   props.setProperty("hibernate.hbm2ddl.auto", props.getProperty("hibernate.hbm2ddl.auto"));
-	   props.setProperty("hibernate.show_sql", props.getProperty("hibernate.show_sql"));
-	   props.setProperty("hibernate.dialect", props.getProperty("hibernate.dialect"));
 	   
 	   return props;
    }
