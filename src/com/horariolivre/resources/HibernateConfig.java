f3c5ba2adbf8325609d2d315458302317a9b200a
Kleber Mota <klebermo@gmail.com>
2017-02-16 11:48:12 -0300

    HibernateConfig.java

--- src/com/horariolivre/resources/HibernateConfig.java
+++ src/com/horariolivre/resources/HibernateConfig.java
@@ -8,7 +8,7 @@ import java.util.Properties;
 
 import javax.sql.DataSource;
 
-import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
